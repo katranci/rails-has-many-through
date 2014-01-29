968e648106ff8b63df62a4f706c5338c92c4c8e3
Ahmet Katrancı <ahmet@katranci.co.uk>
2014-01-29 09:51:58 +0000

    Initialized a new Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsHasManyThrough::Application.config.secret_key_base = 'd8a757f7f34585c62da99a4d81a48f251d63e44aa9bb9845fda25eeac5c4f48e665417ed21ae563a291d356c6cf8279f25c85e868eec1effc040c4f57b668edf'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RailsHasManyThrough::Application.config.session_store :cookie_store, key: '_rails-has-many-through_session'
